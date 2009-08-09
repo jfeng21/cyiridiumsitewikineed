5000fb06f2ddd7dacb87767a3fddb2c95ae72010
iridiumcao <iridiumcao@fa54858a-9f52-0410-afc2-f743258a769a>
2009-08-09 11:01:58 +0000

    simplest Spring app

---
+++ src/hellospring/onlyfun/caterpillar/HelloBean.java
@@ -0,0 +1,19 @@
+package onlyfun.caterpillar;
+
+/**
+ *
+ * @author lgg
+ */
+public class HelloBean {
+
+    private String helloWord;
+
+    public String getHelloWord() {
+        return helloWord;
+    }
+
+    public void setHelloWord(String helloWord) {
+        this.helloWord = helloWord;
+    }
+
+}
