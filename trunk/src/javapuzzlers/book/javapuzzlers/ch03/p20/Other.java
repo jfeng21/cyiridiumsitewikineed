4d5880f9c386164c1303e47c0270fafcf839a691
iridiumcao <iridiumcao@fa54858a-9f52-0410-afc2-f743258a769a>
2009-08-23 10:26:44 +0000

    String.replace, String.replaceAll, Pattern.quote

---
+++ trunk/src/javapuzzlers/book/javapuzzlers/ch03/p20/Other.java
@@ -0,0 +1,12 @@
+package book.javapuzzlers.ch03.p20;
+
+import java.util.regex.Pattern;
+
+public class Other {
+
+	public static void main(String[] args) {
+		System.out.println("aaaaa".replace("aa", "b"));
+		System.out.println(Pattern.quote("."));
+	}
+
+}
