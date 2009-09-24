fd66b3956456e1a3440c65a4a704625311e48eea
iridiumcao <iridiumcao@fa54858a-9f52-0410-afc2-f743258a769a>
2009-09-24 10:03:39 +0000

    added puzzle 21

---
+++ src/javapuzzlers/book/javapuzzlers/ch03/p21/MeToo.java
@@ -0,0 +1,13 @@
+package book.javapuzzlers.ch03.p21;
+
+import java.io.File;
+
+public class MeToo {
+
+	public static void main(String[] args) {
+		System.out.println(MeToo.class.getName().replaceAll("\\.",
+				File.separator)
+				+ ".class");
+	}
+
+}
