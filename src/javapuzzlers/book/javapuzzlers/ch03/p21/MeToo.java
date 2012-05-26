b507cacee832c3cf31c04af1b4ba82e938ba7a94
iridiumcao <iridiumcao@gmail.com>
2012-05-26 23:37:47 +0000

    updated for the related article

--- src/javapuzzlers/book/javapuzzlers/ch03/p21/MeToo.java
+++ src/javapuzzlers/book/javapuzzlers/ch03/p21/MeToo.java
@@ -2,11 +2,26 @@ package book.javapuzzlers.ch03.p21;
 
 import java.io.File;
 
+/**
+ * 
+ * for: https://sites.google.com/site/iridiumsite/it/java/java-lang/book-java-puzzlers/puzzle-21-whats-my-class-take-2
+ *
+ */
 public class MeToo {
 
 	public static void main(String[] args) {
-		System.out.println(MeToo.class.getName().replaceAll("\\.",
-				File.separator)
+//		01. will throw exception
+//		System.out.println(MeToo.class.getName().replaceAll("\\.",
+//				File.separator)
+//				+ ".class");
+		
+//		02. ok
+		System.out.println(MeToo.class.getName().replace(".", File.separator)
+				+ ".class");
+		
+//		03. ok
+		System.out.println(MeToo.class.getName().replace('.',
+				File.separatorChar)
 				+ ".class");
 	}
 
