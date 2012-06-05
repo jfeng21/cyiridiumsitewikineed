dc277b829c162dc584b8d69e4730b8726dcaec4a
iridiumcao <iridiumcao@gmail.com>
2012-06-05 14:44:39 +0000

    简单的逻辑问题：给赛跑者排序

---
+++ src/algorithm/org/iridium/algorithm/wuwenhu/ch4/WhoWinTheRun.java
@@ -0,0 +1,19 @@
+package org.iridium.algorithm.wuwenhu.ch4;
+
+/*
+ * 小狗、小兔、小猫、小猴和小鹿参加百米赛跑，比赛结束后，
+ * 小猴说：“我比小猫跑得快。”
+ * 小狗说：“小鹿在我的前面冲过了终点线。”
+ * 小兔说：“我的名次排在小猴的前面，小狗的后面。”
+ * 
+ * 请根据他们的回答排出名次。
+ * 
+ */
+public class WhoWinTheRun {
+
+	public static void main(String[] args) {
+		String[] animals = { "小狗", "小兔", "小猫", "小猴", "小鹿" };
+		
+	}
+
+}
