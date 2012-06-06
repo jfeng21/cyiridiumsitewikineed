f6af4b0458945f7aa3fb7582192cc5261d467ea7
iridiumcao@gmail.com <iridiumcao@gmail.com@fa54858a-9f52-0410-afc2-f743258a769a>
2012-06-06 14:42:23 +0000

    简单的逻辑问题：给赛跑者排序

--- trunk/src/algorithm/org/iridium/algorithm/wuwenhu/ch4/WhoWinTheRun.java
+++ trunk/src/algorithm/org/iridium/algorithm/wuwenhu/ch4/WhoWinTheRun.java
@@ -12,8 +12,25 @@ package org.iridium.algorithm.wuwenhu.ch4;
 public class WhoWinTheRun {
 
 	public static void main(String[] args) {
-		String[] animals = { "小狗", "小兔", "小猫", "小猴", "小鹿" };
-		
+//		String[] animals = { "小狗", "小兔", "小猫", "小猴", "小鹿" };
+		int a, b, c, d, e = 0;// 对应为小狗, 小兔, 小猫, 小猴, 小鹿的名次
+		for (a = 1; a <= 5; a++) {
+			for (b = 1; b <= 5; b++) {
+				for (c = 1; c <= 5; c++) {
+					for (d = 1; d <= 5; d++) {
+						for (e = 1; e <= 5; e++) {
+							if ((d < c) && (e < a) && (b < d) && (b > a)) {
+								System.out.println("小狗: " + a);
+								System.out.println("小兔: " + b);
+								System.out.println("小猫: " + c);
+								System.out.println("小猴: " + d);
+								System.out.println("小鹿: " + e);
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 }
