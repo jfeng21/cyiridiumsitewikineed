4423447aa420b68408b6c141be571b9a4fb7401d
iridiumcao@gmail.com <iridiumcao@gmail.com@fa54858a-9f52-0410-afc2-f743258a769a>
2012-07-08 14:15:51 +0000

    puzzle 22

---
+++ trunk/src/javapuzzlers/book/javapuzzlers/ch03/p22/DupeOfHttp.java
@@ -0,0 +1,16 @@
+package book.javapuzzlers.ch03.p22;
+
+/**
+ * 
+ * for https://sites.google.com/site/iridiumsite/it/java/java-lang/book-java-puzzlers/puzzle-22-dupe-of-url
+ *
+ */
+public class DupeOfHttp {
+
+	public static void main(String[] args) {
+		System.out.print("Hello, ");
+		http: // www.google.com
+		System.out.println("world!");
+	}
+
+}
