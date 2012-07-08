a5e75d700faecd2580d84369c5d464dd10354181
iridiumcao@gmail.com <iridiumcao@gmail.com@fa54858a-9f52-0410-afc2-f743258a769a>
2012-07-08 03:05:57 +0000

    unicode char

---
+++ trunk/src/helloworld/org/iridium/HelloWorld002.java
@@ -0,0 +1,14 @@
+package org.iridium;
+
+public class HelloWorld002 {
+
+	/**
+	 * \u005b is [
+	 * \u005d is ]
+	 * take care the method argument...
+	 */
+	public static void main(String\u005b\u005d args) {
+		System.out.println("\u005b \u005d");
+	}
+
+}
