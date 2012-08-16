8bcbcb4ca1a0f681faca06a7110a5c61569bf278
iridiumcao@gmail.com <iridiumcao@gmail.com@fa54858a-9f52-0410-afc2-f743258a769a>
2012-08-16 13:40:45 +0000

    puzzle 23: rhymes

---
+++ trunk/src/javapuzzlers/book/javapuzzlers/ch03/p23/Rhymes2.java
@@ -0,0 +1,32 @@
+package book.javapuzzlers.ch03.p23;
+
+import java.util.Random;
+
+/**
+ * 
+ * for https://sites.google.com/site/iridiumsite/it/java/java-lang/book-java-
+ * puzzlers/puzzle-23-no-pain-no-gain
+ */
+public class Rhymes2 {
+	private static Random rnd = new Random();
+
+	public static void main(String[] args) {
+		StringBuffer word = null;
+		switch (rnd.nextInt(2)) {
+		case 1:
+			word = new StringBuffer("P");
+			break;
+		case 2:
+			word = new StringBuffer("G");
+			break;
+		default:
+			word = new StringBuffer("M");
+		}
+		word.append('a');
+		word.append('i');
+		word.append('n');
+
+		System.out.println(word);
+	}
+
+}
