a0cb64399b77fa8e36eb278250a96794681aa61d
iridiumcao <iridiumcao@gmail.com>
2012-12-11 09:13:55 +0000

    增加通项公式的解法

--- src/algorithm/org/iridium/algorithm/basic/Fibonacci.java
+++ src/algorithm/org/iridium/algorithm/basic/Fibonacci.java
@@ -3,16 +3,29 @@ package org.iridium.algorithm.basic;
 public class Fibonacci {
 	public static void main(String[] args) {
 		Fibonacci f = new Fibonacci();
-		for (int i = 1; i < 10; i++) {
-			System.out.println("f(" + i + ") = " + f.fibonnaci(i));
+		for (int i = 1; i < 20; i++) {
+			System.out.print("f(" + i + ") = " + f.fibonnaci(i) + ", ");
+		}
+		
+		System.out.println();
+		
+		for (int i = 1; i < 20; i++) {
+			System.out.print("f(" + i + ") = " + f.fibonnaci2(i) + ", ");
 		}
 	}
 
+	// 递归算法
 	public int fibonnaci(int i) {
 		if (i < 0)
 			return 0;
 		if (i == 1 || i == 2)
 			return 1;
 		return fibonnaci(i - 1) + fibonnaci(i - 2);
+	}
+	
+	// 直接应用通项公式
+	// f(n) = ((1+5^(1/2))^n - (1-5^(1/2))^n)/(2^n * 5^(1/2))
+	public int fibonnaci2(int n) {
+		return (int) ((Math.pow(1 + Math.sqrt(5), n) - Math.pow(1 - Math.sqrt(5), n))/(Math.pow(2, n)*Math.sqrt(5)));
 	}
 }
