3db7f2d76595214608b8b1990960fd24b227a2a7
iridiumcao <iridiumcao@gmail.com>
2012-12-11 08:56:18 +0000

    遞歸方法修正，並保留錯誤的以說明原因

--- src/algorithm/org/iridium/algorithm/basic/EuclidGcd.java
+++ src/algorithm/org/iridium/algorithm/basic/EuclidGcd.java
@@ -4,11 +4,12 @@ public class EuclidGcd {
 
 	public static void main(String[] args) {
 		EuclidGcd e = new EuclidGcd();
-		System.out.println(e.calculateGcd(20, 30));
-		System.out.println(e.calculateGcd2(20, 30));
+		System.out.println(e.calculateGcd(21, 30));
+		System.out.println(e.calculateGcd2(21, 30));//這是一個錯誤值
+		System.out.println(e.calculateGcd2(21, 30));
 	}
 
-	//非遞歸實現
+	// 非遞歸實現
 	int calculateGcd(int m, int n) {
 		if (m < n) {
 			int a = m;
@@ -24,7 +25,8 @@ public class EuclidGcd {
 		return n;
 	}
 
-	//遞歸實現
+	// 遞歸的一個錯誤實現，遞歸調用棧機制導致最終返回了錯誤的值。
+	// 如 m = 21, n = 30 就可以得到錯誤的值。
 	int calculateGcd2(int m, int n) {
 		if (m < n) {
 			int a = m;
@@ -38,5 +40,13 @@ public class EuclidGcd {
 			r = calculateGcd2(m, n);
 		}
 		return n;
+	}
+	
+	// 遞歸實現之二
+	int calculateGcd3(int a, int b) {
+		if (b == 0) {
+			return a;
+		}
+		return this.calculateGcd3(b, a % b);
 	}
 }
