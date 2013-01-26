6aaeb9568bb73499b8c017c947b4b5c15e569aa9
iridiumcao@gmail.com <iridiumcao@gmail.com@fa54858a-9f52-0410-afc2-f743258a769a>
2013-01-26 13:35:39 +0000

    refined comment

--- trunk/src/javapuzzlers/book/javapuzzlers/ch05/p37/Arcane3.java
+++ trunk/src/javapuzzlers/book/javapuzzlers/ch05/p37/Arcane3.java
@@ -14,8 +14,8 @@ interface Type3 extends Type1, Type2 {
 public class Arcane3 implements Type3 {
 	/*
 	 * 这个方法的异常来自 Type1 和 Type2 两个接口，
-	 * 取 CloneNotSupportedException 和 InterruptedException 
-	 * 的交集，此交集为空。
+	 * 抛出的异常类型取 CloneNotSupportedException 和 InterruptedException 
+	 * 的交集，此交集为空，所以这里没有异常抛出。
 	 */
 	public void f() {
 		System.out.println("Hello world");
